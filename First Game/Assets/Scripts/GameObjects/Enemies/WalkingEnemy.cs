202acc121c5a8069624efb4e7b761bd783215a86
Alesteve33 <alex3val@gmail.com>
2023-03-25 12:03:42 +0100

    Walking Enemies functionality

--- First Game/Assets/Scripts/GameObjects/Enemies/WalkingEnemy.cs
+++ First Game/Assets/Scripts/GameObjects/Enemies/WalkingEnemy.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class WalkingEnemy : MonoBehaviour
 {
     Rigidbody2D rb;
+    GameObject player;
+    Collider2D col;
+    
     public float speed;
     int direction = 1;
-    GameObject player;
+    LayerMask groundLayer;
+
+    float maxRightXPos = Mathf.NegativeInfinity;
+    float maxLeftXPos = Mathf.Infinity;
+    bool followPlayer;
 
     void Move()
     {
@@ -18,6 +25,8 @@ public class WalkingEnemy : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
+        col = gameObject.GetComponent<Collider2D>();
+        groundLayer = player.GetComponent<PlayerController>().groundLayer;
     }
 
     // Update is called once per frame
@@ -25,21 +34,58 @@ public class WalkingEnemy : MonoBehaviour
     {
         SetDirection();
         Move();
+        CheckForCorner();
     }
 
     void SetDirection()
     {
-        if(player.transform.position.x > gameObject.transform.position.x)
+        if (player.transform.position.x > gameObject.transform.position.x && followPlayer)
         {
             direction = 1;
         }
-        else if(Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) < .2f)
+        else if (Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) < .2f && followPlayer)
         {
             direction = 0;
         }
-        else
+        else if (followPlayer)
         {
             direction = -1;
         }
+    }
+
+
+    void CheckForCorner()
+    {
+
+        RaycastHit2D leftRay = Physics2D.Raycast(new Vector2(col.bounds.center.x - col.bounds.size.x / 2, col.bounds.center.y), Vector2.down, 10, groundLayer);
+        RaycastHit2D rightRay = Physics2D.Raycast(new Vector2(col.bounds.center.x + col.bounds.size.x / 2, col.bounds.center.y), Vector2.down, 10, groundLayer);
+
+        if (rightRay && !leftRay)
+        {
+            if (rightRay.distance != leftRay.distance)
+            {
+                maxLeftXPos = transform.position.x;
+                followPlayer = false;
+                direction = 1;
+            }
+        }
+        if (!rightRay && leftRay)
+        {
+            if (rightRay.distance != leftRay.distance)
+            {
+                maxRightXPos = transform.position.x;
+                followPlayer = false;
+                direction = -1;
+            }
+        }
+
+        print(player.transform.position.x - maxLeftXPos);
+        print(maxRightXPos - player.transform.position.x);
+
+        if (player.transform.position.x -  maxLeftXPos > 0 && maxRightXPos - player.transform.position.x > 0)
+        {   
+            followPlayer = true;
+        }
+        print(followPlayer);
     }
 }
